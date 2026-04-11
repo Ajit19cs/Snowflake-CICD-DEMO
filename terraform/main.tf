a39b6e8bfb427142fd190c0aee9537bab1e04fbe
20052908 <shridharkhude210@gmail.com>
2026-04-11 21:00:46 +0530

    Clean setup - terraform + schemachange

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,26 @@
-# terraform config will go here
+
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+    }
+  }
+}
+
+provider "snowflake" {
+  organization_name = "nlvnyvj"
+  account_name      = "ww58104"
+
+  user     = "YOGITA24KSHIRSAGAR"
+  password = "Manjirica@2907"
+  role     = "ACCOUNTADMIN"
+}
+
+resource "snowflake_database" "DB_DP_DEV" {
+  name = "DB_DP_DEV"
+}
+
+resource "snowflake_schema" "BR_IN" {
+  database = snowflake_database.DB_DP_DEV.name
+  name     = "BR_IN"
+}
